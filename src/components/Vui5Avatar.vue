e036a3510d380c2b20d70bbfcc4ab23f6fb985b1
ryoluo <40686845+ryoluo@users.noreply.github.com>
2024-08-06 23:53:00 +0900

    First commit of full repository

---
+++ src/components/Vui5Avatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <ui5-avatar></ui5-avatar>
+</template>
+<script lang="ts">
+import "@ui5/webcomponents/dist/Avatar.js";
+
+interface AvatarProps {
+  disable: boolean;
+  interactive: boolean;
+  icon?: string;
+  fallbackIcon: string;
+  initials?: string;
+  shape: "Circle" | "Square";
+  size: "XS" | "S" | "M" | "L" | "XL";
+  colorScheme:
+    | "Accent1"
+    | "Accent2"
+    | "Accent3"
+    | "Accent4"
+    | "Accent5"
+    | "Accent6"
+    | "Accent7"
+    | "Accent8"
+    | "Accent9"
+    | "Accent10"
+    | "Placeholder";
+}
+</script>
